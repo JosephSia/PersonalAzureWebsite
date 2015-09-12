bfc170b5dc161d7cd84f8d617a8926647f52108f
Sia Jin Jun <siajinjun@outlook.com>
2015-09-12 16:59:25 +0800

    1. Initial commit

---
+++ PersonalAzureWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalAzureWebSite.MvcApplication" Language="C#" %>
---
+++ PersonalAzureWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalAzureWebSite.Startup))]
+namespace PersonalAzureWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
